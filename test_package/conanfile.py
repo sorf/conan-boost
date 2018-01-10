52d11622927479931f14a26d5f1431ebb79ecd5d
Sorin Fetche <sorin.fetche@gmail.com>
2018-01-09 23:53:15 -0800

    Update the test_package to build with the 'system' layout of Boost. - the cmake find_package functionality is not used anymore; only the variables coming from conan/package_info

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,6 +20,19 @@ class DefaultNameConan(ConanFile):
             cmake.definitions["HEADER_ONLY"] = "TRUE"
         if not self.options["Boost"].without_python:
             cmake.definitions["WITH_PYTHON"] = "TRUE"
+            if self.settings.os == "Windows":
+                python_folder = os.path.dirname(sys.executable)
+                cmake.definitions["PYTHON_INCLUDE"] = os.path.join(
+                    python_folder,
+                    "include")
+                python_lib_file_name = "python%s%s.lib" % (
+                    sys.version_info.major, sys.version_info.minor)
+                cmake.definitions["PYTHON_LIB"] = os.path.join(
+                    python_folder,
+                    "libs",
+                    python_lib_file_name)
+        if not self.options["Boost"].without_regex:
+            cmake.definitions["WITH_REGEX"] = "TRUE"
         cmake.configure()
         cmake.build()
 
@@ -29,11 +42,14 @@ class DefaultNameConan(ConanFile):
         
     def test(self):        
         data_file = os.path.join(self.conanfile_directory, "data.txt")
+        self.output.info("Running: lambda")
         self.run("cd bin && .%slambda < %s" % (os.sep, data_file))
         if not self.options["Boost"].header_only:
-            self.run("cd bin && .%sregex_exe < %s" % (os.sep, data_file))
-            if self.options["Boost"].python:
+            if not self.options["Boost"].without_regex:
+                self.output.info("Running: regex_exe")
+                self.run("cd bin && .%sregex_exe < %s" % (os.sep, data_file))
+            if not self.options["Boost"].without_python:
                 os.chdir("bin")
                 sys.path.append(".")
                 import hello_ext
-                hello_ext.greet()
+                self.output.info("Calling: Python hello_ext.greet: %s" % hello_ext.greet())
