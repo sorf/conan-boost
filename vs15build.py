c527fdd68ee46ed9474e196717f7b0dcf62fbed0
Sorin Fetche <sorin.fetche@gmail.com>
2018-02-16 07:24:23 -0800

    vs15build.py

---
+++ vs15build.py
@@ -0,0 +1,20 @@
+""" Builds with Visual Studio 2017 (version 15). """
+from conan.packager import ConanMultiPackager
+
+if __name__ == "__main__":
+    visual_studio_version = "15"
+
+    builder_64 = ConanMultiPackager(
+        visual_versions=[visual_studio_version],
+        archs=["x86_64"])
+    builder_64.add_common_builds(shared_option_name="Boost:shared", pure_c=False)
+    #print(builder_64.builds)
+    builder_64.run()
+
+    builder_32_release = ConanMultiPackager(
+        visual_versions=[visual_studio_version],
+        archs=["x86"],
+        build_types=["Release"])
+    builder_32_release.add_common_builds(shared_option_name="Boost:shared", pure_c=False)
+    #print(builder_32_release.builds)
+    builder_32_release.run()
